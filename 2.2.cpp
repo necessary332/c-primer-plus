a99684e361ed3b752ed88f0082b3714f2c0a74fb
necessary332 <18647003873@163.com>
2019-07-07 02:13:01 +0800

    2.1~2.2

---
+++ 2.2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int dist;
+    cout << "enter the distance : ";
+    cin >> dist;
+    cout << "the dist is " << dist << " long / " << dist*220 << " yards." << endl;
+    return 0;
+}
